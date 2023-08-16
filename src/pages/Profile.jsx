c5f28b1736a63eeb9072eb68f2833a183d555c9c
yudhaph0300 <yudhapamungkas0300@gmail.com>
2023-08-16 14:06:13 +0700

    Fol14 - User Sign In

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from "react"
+import { getAuth } from "firebase/auth"
+
 function Profile() {
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [])
+
    return (
-     <div>Profile</div>
-   )
+     user ? <h1>{ user.displayName }</h1> : 'Not Loggin In'
+   ) 
  }
  
  export default Profile

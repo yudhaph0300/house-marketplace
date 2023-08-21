883e10e6b065b8ab6b66f406dbf9f82573287705
yudhaph0300 <yudhapamungkas0300@gmail.com>
2023-08-21 10:16:43 +0700

    Fol14 - Google OAuth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,6 +6,7 @@ import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import OAuth from "../components/OAuth";
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
@@ -108,6 +109,9 @@ function SignUp() {
         </form>
 
         {/* Google Auth */}
+        <OAuth/>
+        
+
         <Link to='/sign-in' className="registerLink">
           Sign In Instead
         </Link>

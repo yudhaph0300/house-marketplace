58d368dd7f0555fc02ddae1ca805fd876076dd64
yudhaph0300 <yudhapamungkas0300@gmail.com>
2023-08-21 13:21:33 +0700

    Fol15 - Listing Item Component

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { collection, getDocs, query, where, orderBy, limit, startAfter } from 'firebase/firestore'
+import { collection, getDocs, query, where, orderBy, limit } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { toast } from "react-toastify"
 import Spinner from '../components/Spinner'
+import ListingItem from "../components/ListingItem"
 
 function Category() {
   const [listings, setListings] = useState(null)
@@ -63,7 +64,11 @@ function Category() {
         <main>
           <ul className="categoryListings">
             {listings.map((listing) => (
-              <h3 key={listing.id}>{listing.data.name}</h3>
+              <ListingItem
+                listing={listing.data}
+                id={listing.id}
+                key={listing.id}
+              />
             ))}
           </ul>
         </main>

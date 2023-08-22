ce5b0c7a223b13756935fcca40101e2e3baa21d4
yudhaph0300 <yudhapamungkas0300@gmail.com>
2023-08-22 15:14:44 +0700

    Fol15 - Uploading Images To FIrebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState, useRef } from "react"
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { db } from '../firebase.config'
 import { useNavigate } from "react-router-dom"
 import Spinner from '../components/Spinner'
 import { toast } from "react-toastify"
+import { v4 as uuidv4 } from "uuid"
 
 function CreateListing() {
    const [geolocationEnabled, setGeolocationEnabled] = useState(true)
@@ -78,34 +81,87 @@ function CreateListing() {
          return
       }
      
-      let geolocation = {}
-      let location
+      // let geolocation = {}
+      // let location
      
-      if (geolocationEnabled) {
-         const response = await fetch(
-            `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
-         )
+      // if (geolocationEnabled) {
+      //    const response = await fetch(
+      //       `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      //    )
    
-         const data = await response.json()
+      //    const data = await response.json()
    
-         geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
-         geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+      //    geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+      //    geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
    
-         location =
-            data.status === 'ZERO_RESULTS'
-            ? undefined
-            : data.results[0]?.formatted_address
+      //    location =
+      //       data.status === 'ZERO_RESULTS'
+      //       ? undefined
+      //       : data.results[0]?.formatted_address
    
-         if (location === undefined || location.includes('undefined')) {
-            setLoading(false)
-            toast.error('Please enter a correct address')
-            return
-         }
-      } else {
-         geolocation.lat = latitude
-         geolocation.lng = longitude
-      }
+      //    if (location === undefined || location.includes('undefined')) {
+      //       setLoading(false)
+      //       toast.error('Please enter a correct address')
+      //       return
+      //    }
+      // } else {
+      //    geolocation.lat = latitude
+      //    geolocation.lng = longitude
+      // }
 
+      // Store image in firebase
+      const storeImage = async (image) => {
+         return new Promise((resolve, reject) => {
+            const storage = getStorage()
+            const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+            const storageRef = ref(storage, 'images/' + fileName)
+
+            const uploadTask = uploadBytesResumable(storageRef, image)
+
+            uploadTask.on(
+               'state_changed',
+               (snapshot) => {
+                  const progress =
+                     (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                  console.log('Upload is ' + progress + '% done')
+                  switch (snapshot.state) {
+                     case 'paused':
+                        console.log('Upload is paused')
+                        break
+                     case 'running':
+                        console.log('Upload is running')
+                        break
+                     default:
+                        break
+                  }
+               },
+
+               (error) => {
+                  reject(error)
+               },
+
+               () => {
+                  // Handle successful uploads on complete
+                  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                  getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                     resolve(downloadURL)
+                  })
+               }
+            )
+         })
+      }
+      
+      const imgUrls = await Promise.all(
+         [...images].map((image) => storeImage(image))
+      ).catch(() => {
+         setLoading(false)
+         toast.error('Images not uploaded')
+         return
+      })
+
+      console.log(imgUrls)
+      
       setLoading(false)
    }
 

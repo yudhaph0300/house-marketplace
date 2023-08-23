d818ed6bed17c9b7f8d200afdbbc1fac0d2ece2d
yudhaph0300 <yudhapamungkas0300@gmail.com>
2023-08-23 13:56:58 +0700

    Fol16 - Listing page slider

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,40 +1,60 @@
-import { useState, useEffect } from "react"
-import { Link, useNavigate, useParams } from "react-router-dom"
-import { getDoc, doc } from "firebase/firestore"
-import { getAuth } from "firebase/auth"
-import { db } from "../firebase.config"
-import Spinner from "../components/Spinner"
+import { useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+import { getDoc, doc } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { db } from '../firebase.config'
+import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
-   const [listing, setListing] = useState(null)
-   const [loading, setLoading] = useState(true)
-   const [shareLinkCopied, setShareLinkCopied] = useState(false)
+  const [listing, setListing] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [shareLinkCopied, setShareLinkCopied] = useState(false)
 
-   const navigate = useNavigate()
-   const params = useParams()
-   const auth = getAuth()
+  const navigate = useNavigate()
+  const params = useParams()
+  const auth = getAuth()
 
-   useEffect(() => {
-      const fetchListing = async () => {
-         const docRef = doc(db, 'listings', params.listingId)
-         const docSnap = await getDoc(docRef)
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, 'listings', params.listingId)
+      const docSnap = await getDoc(docRef)
 
-         if (docSnap.exists()) {
-            console.log(docSnap.data())
-            setListing(docSnap.data())
-            setLoading(false)
-         }
+      if (docSnap.exists()) {
+        setListing(docSnap.data())
+        setLoading(false)
       }
+    }
 
-      fetchListing()
-   }, [navigate, params.listingId])
+    fetchListing()
+  }, [navigate, params.listingId])
 
-   if (loading) {
-      return <Spinner/>
-   }
+  if (loading) {
+    return <Spinner />
+  }
+
   return (
     <main>
+      
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imageUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imageUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+              }}
+              className='swiperSlideDiv'
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+
       <div className="shareIconDiv" onClick={() => {
          navigator.clipboard.writeText(window.location.href)
          setShareLinkCopied(true)
@@ -42,7 +62,7 @@ function Listing() {
             setShareLinkCopied(false)
          }, 2000)
       }}>
-         <img src={shareIcon} alt="" />
+         <image src={shareIcon} alt="" />
       </div>
       {shareLinkCopied && <p className="linkCopied">Link Copied!</p>}
 
